4f78a3b1f178a1863299f0b72fb47ef5b44b2e06
bennithor <bennithor@gmail.com>
2015-12-17 16:36:48 +0000

    "Fixed AbilitTargeting"

--- btest.py
+++ btest.py
@@ -1,3 +1,27 @@
 import AbilityTargeting
+import Creature
+import CombatTeam
+import Combat
+import Ability
+import CombatUI
+import AbilityEffect
+attack = Ability.Ability('Attack',5,1)
+effAttack = AbilityEffect.AbilityEffect('basicAttack',0,0,1,1)
+attack.AddEffect(effAttack)
+c1 = Creature.Creature("Steinar")
+c1.AddAbility(attack)
+c2 = Creature.Creature("Benni")
+c2.AddAbility(attack)
+c1.AddAttributes(1)
+playerTeam = CombatTeam.CombatTeam()
+playerTeam.AddToTeam(c1)
+
+enemyTeam = CombatTeam.CombatTeam()
+enemyTeam.AddToTeam(c2)
 
 
+userInt = CombatUI.CombatUI(playerTeam,enemyTeam)
+
+k =0
+while k==0:
+	userInt.MainScreen()
